b83157627ded3f27b1cc351c71d8521e8476c14a
Wataru0530 <109639652+Wataru0530@users.noreply.github.com>
2022-08-22 14:38:07 +0900

    アソシエーション、バリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,18 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
   has_one_attached :image
 
-  
+  validate :item_name,    presence: true
+  validate :info,         presence: true
+  validate :category_id,  presence: true
+  validate :situation_id, presence: true
+  validate :cost_id,      presence: true
+  validate :area_id,      presence: true
+  validate :day_id,       presence: true
+  validate :price,        presence: true
+  validate :user,         presence: true
+
+  validate :genre_id, numericality: { other_than: 1 }
+
 end
