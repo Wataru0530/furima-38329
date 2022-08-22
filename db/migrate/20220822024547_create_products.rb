3c026596d6530cfa58f211d5766f1a82dfbaae43
Wataru0530 <109639652+Wataru0530@users.noreply.github.com>
2022-08-22 21:35:36 +0900

    修正

--- db/migrate/20220822024547_create_products.rb
+++ db/migrate/20220822024547_create_products.rb
@@ -11,7 +11,6 @@ class CreateProducts < ActiveRecord::Migration[6.0]
       t.integer     :day_id,       null: false
       t.integer     :price,        null: false
       t.references  :user,         null: false, foreign_key: true
-      t.integer     :category_id,  null: false
       
 
       t.timestamps
