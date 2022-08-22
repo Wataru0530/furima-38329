0a14c3f1cbe17e3e96db85b874477143a2457820
Wataru0530 <109639652+Wataru0530@users.noreply.github.com>
2022-08-22 12:25:51 +0900

    テーブル作成、アクティブレコードの導入

---
+++ db/migrate/20220822024547_create_products.rb
@@ -0,0 +1,18 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+
+      t.string      :item_name,    null: false 
+      t.text        :info,         null: false
+      t.integer     :category_id,  null: false
+      t.integer     :situation_id, null: false 
+      t.integer     :cost_id,      null: false
+      t.integer     :area_id,      null: false
+      t.integer     :day_id,       null: false
+      t.integer     :price,        null: false
+      t.references  :user,         null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
