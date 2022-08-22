505061b2719cf5b43aa91664b71f3d6ae2202e70
Wataru0530 <109639652+Wataru0530@users.noreply.github.com>
2022-08-22 20:11:15 +0900

    プルダウンの際の表示内容

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,20 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category_id
+  belongs_to :situation_id
+  belongs_to :cost_id
+  belongs_to :area_id
+  belongs_to :day_id
   has_one_attached :image
 
-  validate :item_name,    presence: true
-  validate :info,         presence: true
-  validate :category_id,  presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validate :situation_id, presence: true
-  validate :cost_id,      presence: true
-  validate :area_id,      presence: true
-  validate :day_id,       presence: true
-  validate :price,        presence: true
-  validate :user,         presence: true
+  validates :item_name,    presence: true
+  validates :info,         presence: true
+  validates :category_id,  presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :situation_id, presence: true
+  validates :cost_id,      presence: true
+  validates :area_id,      presence: true
+  validates :day_id,       presence: true
+  validates :price,        presence: true
+  validates :user,         presence: true
 
 end
