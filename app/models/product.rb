2705e204a514f165c8e24b1c286a7c5fe5fa830e
Wataru0530 <109639652+Wataru0530@users.noreply.github.com>
2022-08-22 17:23:57 +0900

    修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ApplicationRecord
 
   validate :item_name,    presence: true
   validate :info,         presence: true
-  validate :category_id,  presence: true
+  validate :category_id,  presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validate :situation_id, presence: true
   validate :cost_id,      presence: true
   validate :area_id,      presence: true
@@ -13,5 +13,4 @@ class Product < ApplicationRecord
   validate :price,        presence: true
   validate :user,         presence: true
 
-  validate :category_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
