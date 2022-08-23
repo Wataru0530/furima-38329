6486f838a37071af80f420e85f199d1eee7ced8d
Wataru0530 <109639652+Wataru0530@users.noreply.github.com>
2022-08-23 17:44:32 +0900

    幅の調整

--- db/migrate/20220822024547_create_products.rb
+++ db/migrate/20220822024547_create_products.rb
@@ -1,7 +1,6 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-
       t.string      :item_name,    null: false 
       t.text        :info,         null: false
       t.integer     :category_id,  null: false
@@ -11,8 +10,6 @@ class CreateProducts < ActiveRecord::Migration[6.0]
       t.integer     :day_id,       null: false
       t.integer     :price,        null: false
       t.references  :user,         null: false, foreign_key: true
-      
-
       t.timestamps
     end
   end
