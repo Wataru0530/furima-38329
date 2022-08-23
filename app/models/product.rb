6b01c432c85b855a5dc94233ff2c2fc15c4ad82c
Wataru0530 <109639652+Wataru0530@users.noreply.github.com>
2022-08-23 17:46:30 +0900

    アソシエーションの修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,17 +6,17 @@ class Product < ApplicationRecord
   belongs_to :area_id
   belongs_to :day_id
   has_one_attached :image
+  belongs_to :user
 
   validates :item_name,    presence: true, length: { maximum: 40 }
   validates :info,         presence: true, length: { maximum: 1000 }
-  validates :category_id,  presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :situation_id, presence: true
-  validates :cost_id,      presence: true
-  validates :area_id,      presence: true
-  validates :day_id,       presence: true
-  validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
-  validates :user,         presence: true
+  validates :category_id,  presence: true, numericality: { other_than: 1}
+  validates :situation_id, presence: true, numericality: { other_than: 1}
+  validates :cost_id,      presence: true, numericality: { other_than: 1}
+  validates :area_id,      presence: true, numericality: { other_than: 1}
+  validates :day_id,       presence: true, numericality: { other_than: 1}
+  validates :price,        presence: true, numericality: { with: /\A[0-9]+\z/}
+  validates :price,        presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
   validates :image,        presence: true
 
 end
