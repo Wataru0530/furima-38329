ec319969c73c605dc756386b530d5381ba1bc06f
Wataru0530 <109639652+Wataru0530@users.noreply.github.com>
2022-08-29 16:35:18 +0900

    バリデーションの設定

---
+++ app/models/edit.rb
@@ -0,0 +1,23 @@
+class Edit < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :situation
+  belongs_to :cost
+  belongs_to :area
+  belongs_to :day
+  belongs_to :user
+  has_one_attached :image
+  belongs_to :product
+
+  validates :item_name,    presence: true, length: { maximum: 40 }
+  validates :info,         presence: true, length: { maximum: 1000 }
+  validates :category_id,  numericality: { other_than: 1}
+  validates :situation_id, presence: true, numericality: { other_than: 1}
+  validates :cost_id,      presence: true, numericality: { other_than: 1}
+  validates :area_id,      presence: true, numericality: { other_than: 1}
+  validates :day_id,       presence: true, numericality: { other_than: 1}
+  validates :price,        presence: true, numericality: { with: /\A[0-9]+\z/, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
+  validates :image,        presence: true
+
+  
+end
