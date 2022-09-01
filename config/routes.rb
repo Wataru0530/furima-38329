3b1e137009522853c6662c341053e19f0e7cdfcf
Wataru0530 <109639652+Wataru0530@users.noreply.github.com>
2022-09-01 16:50:47 +0900

    名称の変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: 'furimas#index'
-  resources :furimas
-  
+  root to: 'products#index'
+
+  resources :products do
+    resources :informations, only: [:index, :create]
+  end
 end
--- test/controllers/furimas_controller_test.rb
+++ test/controllers/furimas_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class FurimasControllerTest < ActionDispatch::IntegrationTest
+class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get furimas_index_url
+    get products_index_url
     assert_response :success
   end
 
