b765df042957b936460109094a3c2337a98609e9
Wataru0530 <109639652+Wataru0530@users.noreply.github.com>
2022-09-01 16:55:49 +0900

    バリデーションの設定

--- app/models/information_residence.rb
+++ app/models/information_residence.rb
@@ -1,3 +1,23 @@
-class InformationRegidence
+class InformationResidence
   include ActiveModel::Model
-  attr_accessor :card, :deadline, :security, :code, :area_id, :city, :address, :phone
+  attr_accessor :user_id, :product_id, :code, :area_id, :city, :address,:building, :phone, :token
+
+  with_options presence: true do
+    validates :user_id
+    validates :product_id
+    validates :code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :area_id
+    validates :city
+    validates :address
+    validates :phone, format: { with: /\A\d{10,11}\z/ }
+    validates :token
+  end
+
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  def save
+    information = Information.create(user_id: user_id, producf_id: product_id)
+    Residence.create(code: code, area_id: area_id, city: city, address: address, building: building,
+                           phone: phone, buyer_id: buyer.id)
+  end
+end
--- db/migrate/20220831064058_create_information.rb
+++ db/migrate/20220831064058_create_information.rb
@@ -1,9 +1,8 @@
 class CreateInformation < ActiveRecord::Migration[6.0]
   def change
     create_table :information do |t|
-      t.integer        :card,     null: false
-      t.integer        :deadline, null: false
-      t.integer        :security, null: false
+      t.references :user, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
       t.timestamps
     end
   end
