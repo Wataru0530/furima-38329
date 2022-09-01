e578a2b6c04ee1006501b4318adbfd6a042221c1
Wataru0530 <109639652+Wataru0530@users.noreply.github.com>
2022-09-01 16:53:55 +0900

    コントローラーの設定

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,58 @@
+class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :contributor_confirmation, only: [:destroy]
+
+  def index
+    @products = Product.all.order(created_at: "DESC")
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def show
+  end
+  
+  def edit
+    redirect_to root_path unless current_user.id == @product.user.id
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to furima_path(@product.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @product.destroy
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:item_name, :image, :info, :category_id, :situation_id, :cost_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @product.user
+  end
+
+end
