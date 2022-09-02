281abae196f5a23dfb10fe65d2d6010c2dbb4516
Wataru0530 <109639652+Wataru0530@users.noreply.github.com>
2022-09-02 13:22:51 +0900

    テストコードの記述

--- spec/factories/information_residence.rb
+++ spec/factories/information_residence.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     city          { '横浜市緑区' }
     address       { '青山1-1-1' }
     building      { '柳ビル103' }
-    phone_number  { '09012345678' }
+    phone         { '09012345678' }
     token         {"tok_abcdefghijk00000000000000000"}
   end
 end
